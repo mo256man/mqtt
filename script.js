b86d19c70ab0ae2049c9bc31b0cb08445d5d02e4
mo256man <48610559+mo256man@users.noreply.github.com>
2025-01-04 23:55:54 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const brokerUrl = "wss://broker.emqx.io:8084/mqtt";
-const CLIENT_ID = "github-client-" + Math.floor(Math.random() * 999);
-const topic = "python/mqtt";
+const CLIENT_ID = "myClient-" + Math.floor(Math.random() * 999);
+const topic = "myTopic";
 
 const client = mqtt.connect(brokerUrl, {
     username: "mo256man",
@@ -8,23 +8,28 @@ const client = mqtt.connect(brokerUrl, {
     clientId: CLIENT_ID,
 });
 
+document.getElementById("clientID").innerText = CLIENT_ID;
 
 // 接続イベント
 client.on("connect", () => {
     console.log("Connected to EMQX broker (SSL)");
     client.subscribe(topic, (err) => {
-    if (!err) {
-        console.log(`Subscribed to topic: ${topic}`);
-    } else {
-        console.error("Failed to subscribe:", err);
-    }
+        if (!err) {
+            console.log(`Subscribed to topic: ${topic}`);
+        } else {
+            console.error("Failed to subscribe:", err);
+        }
     });
 });
 
 // メッセージ送信
 const publishMessage = (content) => {
+    const now = cdate();
+    const str_now = now.format("YYYY/MM/DD HH:mm:ss");
+    console.log(str_now);
     const message = JSON.stringify({
         clientId: CLIENT_ID,
+        datetime: str_now,
         content: content,
     });
     client.publish(topic, message, (err) => {
@@ -32,6 +37,8 @@ const publishMessage = (content) => {
             console.log("Message published:", content);
         }
     });
+    const html = `<span style ="blue">${str_now} send ${content}</span>`;
+
 }
 
 
